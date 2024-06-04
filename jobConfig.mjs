9a3b508f77109da5e31ddb91c3200b30279d5908
yashmulik1278 <153894472+yashmulik1278@users.noreply.github.com>
2024-06-04 15:38:44 +0530

    Add files via upload
    Take 4

---
+++ jobConfig.mjs
@@ -0,0 +1,19 @@
+//jobConfig.js
+//New syntax
+export default [
+    {
+      index: 1,
+      nameOfReport: 'XYZ',
+      dbQueryApiName: 'getXYZAPIData',
+      jobTimes: '10:00',
+      durationInMins: null,
+    },
+    {
+      index: 2,
+      nameOfReport: 'ABC',
+      dbQueryApiName: 'getABCAPIData',
+      jobTimes: '11:00',
+      durationInMins: 10,
+    },
+  ];
+  
---
+++ logger.mjs
@@ -0,0 +1,12 @@
+// logger.js
+import log4js from 'log4js';
+
+log4js.configure({
+  appenders: { scheduler: { type: 'file', filename: 'scheduler.log', maxLogSize: 10485760, backups: 7, compress: true } },
+  categories: { default: { appenders: ['scheduler'], level: 'info' } }
+});
+
+const logger = log4js.getLogger('scheduler');
+
+//Newer syntax
+export default logger;
