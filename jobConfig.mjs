0e923057e76f49c21e33d2f591c685f5f522ebdc
yashmulik1278 <153894472+yashmulik1278@users.noreply.github.com>
2024-06-05 22:25:50 +0530

    Add files via upload

---
+++ jobConfig.mjs
@@ -0,0 +1,13 @@
+// jobConfig.js
+// New syntax
+export default [
+  {
+    index: 1,
+    reportName: 'UPI Transactions',
+    apiName: 'upitxns',
+    jobTimes: '22:19',
+    durationInMins: null,
+    groupId: "120363305638094699@g.us"
+  }
+];
+
